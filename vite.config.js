4ae63e9e2e77ab0b2c2aee0a8a7ba521461c3a29
mmacedot <mmacedot@ulasalle.edu.pe>
2026-06-11 20:04:44 -0500

    Fix: Implementación de variables globales y bypass CORS en producción

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   preview: {
     allowedHosts: true, 
-    // ESTA ES LA LÍNEA LITERAL QUE FALTA (Asegura que Railway asigne el puerto numérico correcto)
+    // Asegura que Railway asigne el puerto numérico dinámico correcto en producción
     port: parseInt(process.env.PORT) || 4173,
     host: '0.0.0.0'
   }
